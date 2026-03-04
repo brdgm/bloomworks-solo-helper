ab9a9e9fd00708cf4aa66e3ef4d970f91504f5c7
Stefan Seifert <stefanseifert@users.noreply.github.com>
2026-03-04 21:04:22 +0100

    setup screen

--- src/views/SetupBot.vue
+++ src/views/SetupBot.vue
@@ -1,19 +1,29 @@
 <template>
   <h1>{{t('setupBot.title')}}</h1>
 
-  <p>...</p>
+  <div class="mt-3 instructions">
+    <p>{{t('setupBot.instructions.intro')}}</p>
+    <ol>
+      <li>{{t('setupBot.instructions.ladyPeiNoStartingResources')}}</li>
+      <li>{{t('setupBot.instructions.youAreFirstPlayer', {startingMoney})}}</li>
+      <li>{{t('setupBot.instructions.chooseSetupCard')}}</li>
+      <li>{{t('setupBot.instructions.ladyPeiStartingGarden')}}</li>
+    </ol>
+  </div>
 
   <button class="btn btn-primary btn-lg mt-4" @click="startGame()">
     {{t('action.startGame')}}
   </button>
 
-  <FooterButtons endGameButtonType="abortGame"/>
+  <FooterButtons backButtonRouteTo="/setupGame" endGameButtonType="abortGame"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
 import FooterButtons from '@/components/structure/FooterButtons.vue'
+import { useStateStore } from '@/store/state'
+import BotMode from '@/services/enum/BotMode'
 
 export default defineComponent({
   name: 'SetupBot',
@@ -22,7 +32,16 @@ export default defineComponent({
   },
   setup() {
     const { t } = useI18n()
-    return { t }
+    const state = useStateStore()
+    return { t, state }
+  },
+  computed: {
+    startingMoney() : number {
+      if (this.state.setup.botMode == BotMode.ADVANCED) {
+        return 6
+      }
+      return 8
+    }
   },
   methods: {
     startGame() : void {
@@ -31,3 +50,12 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.instructions {
+  max-width: 1000px;
+  ol > li {
+    margin-top: 0.5rem;
+  }
+}
+</style>
